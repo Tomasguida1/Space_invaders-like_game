1e3707b3248a7efd0bbac9d57a69fd875d976f08
Tomas Guida <tomasguida2000@hotmail.com>
2022-09-21 19:12:35 -0300

    background added

--- invasores_del_espacio.py
+++ invasores_del_espacio.py
@@ -11,6 +11,7 @@ pantalla = pygame.display.set_mode((800,600))
 pygame.display.set_caption("invasores del espacio")
 icono = pygame.image.load("alien.png")
 pygame.display.set_icon(icono)
+fondo = pygame.image.load("fondo.jpg")
 
 #crear jugador
 img_player = pygame.image.load("spaceship.png")
@@ -22,7 +23,7 @@ player_x_cambio = 0
 img_enemy = pygame.image.load("enemy.png")
 enemy_x = random.randint(0,734)
 enemy_y = random.randint(50, 200)
-enemy_x_cambio = 0.3
+enemy_x_cambio = 0.5
 enemy_y_cambio = 50
 
 #funcion del jugador
@@ -37,7 +38,8 @@ def enemy(x, y):
 se_ejecuta = True
 #iteracion de eventos
 while se_ejecuta:
-    pantalla.fill((77,0,255))
+    #imagen
+    pantalla.blit(fondo, (0, 0))
     
     #cerrar el juego
     for evento in pygame.event.get():
@@ -47,9 +49,9 @@ while se_ejecuta:
         #chequea si se presiona una flecha 
         if evento.type == pygame.KEYDOWN:
             if evento.key == pygame.K_LEFT:
-                player_x_cambio -= 0.3
+                player_x_cambio -= 0.5
             if evento.key == pygame.K_RIGHT:
-                player_x_cambio = 0.3
+                player_x_cambio = 0.5
         
         #chequea si suelta las flechas
         if evento.type == pygame.KEYUP:
@@ -67,10 +69,10 @@ while se_ejecuta:
     enemy_x += enemy_x_cambio
     #mantener entre los bordes al enemigo
     if enemy_x <= 0:
-        enemy_x_cambio = 0.3
+        enemy_x_cambio = 0.5
         enemy_y += enemy_y_cambio
     elif enemy_x >= 734:
-        enemy_x_cambio = -0.3
+        enemy_x_cambio = -0.5
         enemy_y += enemy_y_cambio
     
     enemy(enemy_x,enemy_y)   
